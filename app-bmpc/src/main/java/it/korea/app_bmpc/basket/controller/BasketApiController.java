de924676c04280baa5fd9e22186fdebf29142caf
HansolJung <solsol3638@naver.com>
2025-10-28 14:17:09 +0900

    POST -> PUT으로 변경

--- app-bmpc/src/main/java/it/korea/app_bmpc/basket/controller/BasketApiController.java
+++ app-bmpc/src/main/java/it/korea/app_bmpc/basket/controller/BasketApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -151,7 +152,7 @@ public class BasketApiController {
      * @throws Exception
      */
     @PreAuthorize("hasRole('USER')")
-    @PostMapping("/basket/user/{userId}/item/{basketItemId}/increase")
+    @PutMapping("/basket/user/{userId}/item/{basketItemId}/increase")
     public ResponseEntity<?> increaseMenuQuantity(
             @PathVariable(name = "userId") String userId,
             @PathVariable(name = "basketItemId") int basketItemId,
@@ -175,7 +176,7 @@ public class BasketApiController {
      * @throws Exception
      */
     @PreAuthorize("hasRole('USER')")
-    @PostMapping("/basket/user/{userId}/item/{basketItemId}/decrease")
+    @PutMapping("/basket/user/{userId}/item/{basketItemId}/decrease")
     public ResponseEntity<?> decreaseMenuQuantity(
             @PathVariable(name = "userId") String userId,
             @PathVariable(name = "basketItemId") int basketItemId,
